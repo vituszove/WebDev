127015f8d828a6539b8738ee730eda731f99ff3e
vituszove <vituszove@gmail.com>
2019-12-16 05:27:21 +0800

    update mongoose dependencies

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const auth = require("../../middleware/auth");
 const Post = require("../../models/Post");
-const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 //@route   POST api/posts
 //@desc    Create a post
